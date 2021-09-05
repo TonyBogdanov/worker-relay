8f70472de592ac86003de204439d50845a270af3
Tony Bogdanov <tonybogdanov@gmail.com>
2021-09-05 13:19:21 +0300

    fix: missing export

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import registerResolver from './src/register-resolver';
 import registerFrontend from './src/frontend/register-frontend';
 import registerBackend from './src/backend/register-backend';
 import runFrontend from './src/frontend/run-frontend';
 import runBackend from './src/backend/run-backend';
 
-export { registerFrontend, registerBackend, runFrontend, runBackend };
+export { registerResolver, registerFrontend, registerBackend, runFrontend, runBackend };
